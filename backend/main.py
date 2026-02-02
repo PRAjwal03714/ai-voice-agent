6163bb2a5b2d60f1c2ae5caef7b9dfdfa5fc171b
PRAjwal03714 <vprajwal714@gmail.com>
2026-02-01 23:16:24 -0500

    added CORS

--- backend/main.py
+++ backend/main.py
@@ -20,11 +20,13 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://ai-voice-agent-chi.vercel.app","https://ai-voice-agent-prajwal03714s-projects.vercel.app", "https://ai-voice-agent-git-production-prajwal03714s-projects.vercel.app", "https://ai-voice-agent-cypq7itbo-prajwal03714s-projects.vercel.app"],
+    allow_origins=["http://localhost:3000"],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allow all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 
 # Initialize database on startup
 
