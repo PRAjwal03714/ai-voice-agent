e36d1fe2556d176876ded50e9b2fb5f93b1fa7fd
PRAjwal03714 <vprajwal714@gmail.com>
2026-02-02 20:07:16 -0500

    Mongodb url

--- backend/mongodb.py
+++ backend/mongodb.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # MongoDB connection
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+MONGO_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
 client = AsyncIOMotorClient(MONGO_URL)
 db = client.vanessa_ai  # Database name
 transcripts_collection = db.transcripts  # Collection name
